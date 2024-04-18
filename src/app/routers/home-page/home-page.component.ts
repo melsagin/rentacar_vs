3d5600cae99f5180319dc434fcf66f95ed9229d9
Melsa Sağın <melsa.sagin@gmail.com>
2024-04-18 17:59:41 +0300

    add first commit

---
+++ src/app/routers/home-page/home-page.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MainLayoutComponent } from '../../shared/layouts/main-layout/main-layout.component';
+import { ModelsListComponent } from '../../features/models/components/models-list/models-list.component';
+import { BrandsListComponent } from '../../features/models/components/brands-list/brands-list.component';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MainLayoutComponent,
+    ModelsListComponent,
+    BrandsListComponent,
+  ],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+})
+export class HomePageComponent {}
